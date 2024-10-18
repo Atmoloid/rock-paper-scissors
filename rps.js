fb1b9da536030f7ff4765b06eaca19bb6890bfa6
Lorenzo <lorenzopica31@gmail.com>
2024-10-18 14:33:22 +0200

    fixed minor spelling bug

--- rps.js
+++ rps.js
@@ -28,29 +28,7 @@ function updateResult(result) {
 function computerResult(result){
     computerScoreSpan.textContent = result;
 }
-/*function getHumanChoice(){
-   
-    /*let repeat = true;
-    let choice = "";
 
-    while (repeat){
-        choice = prompt("Input your choice:");
-        if (choice !== null){
-
-            choice = choice.toLowerCase();
-            if (choice === "rock" || choice === "paper" || choice ==="scissors"){
-                repeat = false
-            }else{
-                alert("Error: Invalid choice! Please choose 'rock', 'paper', or 'scissors'.");
-            }
-        }
-        else{alert("Input cancelled. Please refresh to try again.");} 
-    }
-   
-    //return choice
-    
-};*/
-//getComputerChoice();
 let  humanScore = 0;
   let  computerScore = 0;
 function playRound(humanChoice,computerChoice){
@@ -75,24 +53,3 @@ function playRound(humanChoice,computerChoice){
     updateResult(humanScore);
     computerResult(computerScore);
 }
-/*playRound(/*humanSelection, computerSelection);*/
-
-
-/*function playGame(){
-   humanScore = 0;
-   computerScore = 0;
-    for(let i = 0; i < 5; i++){
-        const computerSelection = getComputerChoice();
-        const humanSelection = humanChoice;
-    playRound(humanSelection, computerSelection);
-};
-console.log(`Final Score -> Human: ${humanScore}, Computer: ${computerScore}`);
-if (humanScore > computerScore) {
-    console.log("YOU WIN THE GAME!");
-} else if (computerScore > humanScore) {
-    console.log("YOU LOSE THE GAME!");
-} else {
-    console.log("THE GAME IS A DRAW!");
-}
-}
-playGame();*/
